f5bad27f4d7695db3b6e7b637d3715ec108f7707
omal0 <nate.crivelli@gmail.com>
2026-05-04 13:30:53 -0700

    interpreter iwth recursive parsing and comments

--- main.py
+++ main.py
@@ -6,108 +6,129 @@ statements = []
 def count_clucks(tokens):
     return sum(1 for t in tokens if t == "cluck")
 
-def execute(statements, variables):
+
+def execute(statements, state):
+    tape = state["tape"]
+    labels = state["labels"]
+
     for stmt in statements:
         kind = stmt[0]
 
         if kind == "nest":
             name = stmt[1]
-            variables[name] = 0
+            labels[name] = state["pointer"]
+
+        elif kind == "roam":
+            name = stmt[1]
+            state["pointer"] = labels[name]
 
         elif kind == "feed":
-            name = stmt[1]
-            value = stmt[2]
-            variables[name] = value
+            value = stmt[1]
+            tape[state["pointer"]] = value
 
         elif kind == "bok":
-            name = stmt[1]
-            variables[name] += 1
+            tape[state["pointer"]] += 1
 
         elif kind == "peck":
-            name = stmt[1]
-            variables[name] -= 1
+            tape[state["pointer"]] -= 1
+
+        elif kind == "strut":
+            state["pointer"] += 1
+
+        elif kind == "shuffle":
+            state["pointer"] -= 1
 
         elif kind == "cluck":
-            name = stmt[1]
-            print(variables[name])
+            print(tape[state["pointer"]])
 
         elif kind == "loop":
-            name = stmt[1]
-            body = stmt[2]
-            while variables[name] != 0:
-                execute(body, variables)
+            body = stmt[1]
+            while tape[state["pointer"]] != 0:
+                execute(body, state)
 
-if (len(sys.argv) < 2):
-    print("Error: python chicken <filename>")
-    sys.exit(0)
+def parse_block(lines, i=0, inside_loop=False):
+    statements = []
+
+    while i < len(lines):
+        line = lines[i]
+        line = line.split("#")[0].strip()
+
+        if not line:
+            i += 1
+            continue
+
+        tokens = line.split()
+        cmd = tokens[0]
+
+        if cmd == "roost":
+            if inside_loop:
+                return statements, i + 1
+            else:
+                print("Error: roost without coop")
+                sys.exit(1)
+
+        elif cmd == "coop":
+            body, i = parse_block(lines, i + 1, inside_loop=True)
+            statements.append(("loop", body))
+
+        elif cmd == "nest":
+            statements.append(("nest", tokens[1]))
+            i += 1
+
+        elif cmd == "roam":
+            statements.append(("roam", tokens[1]))
+            i += 1
+
+        elif cmd == "feed":
+            value = count_clucks(tokens[1:])
+            statements.append(("feed", value))
+            i += 1
+
+        elif cmd == "bok":
+            statements.append(("bok",))
+            i += 1
+
+        elif cmd == "peck":
+            statements.append(("peck",))
+            i += 1
+
+        elif cmd == "strut":
+            statements.append(("strut",))
+            i += 1
+
+        elif cmd == "shuffle":
+            statements.append(("shuffle",))
+            i += 1
+
+        elif cmd == "cluck":
+            statements.append(("cluck",))
+            i += 1
+
+        else:
+            print(f"Unknown command: {line}")
+            sys.exit(1)
+
+    if inside_loop:
+        print("Error: missing roost")
+        sys.exit(1)
+
+    return statements, i
+
+if len(sys.argv) < 2:
+    print("Error: python main.py <filename>")
+    sys.exit(1)
 
 filename = sys.argv[1]
 
 with open(filename, "r") as file:
     lines = file.readlines()
 
-i = 0
-while i < len(lines):
-    line = lines[i].strip()
+statements, _ = parse_block(lines)
 
-    if not line:
-        i += 1
-        continue
+state = {
+    "tape": [0] * 30000,
+    "pointer": 0,
+    "labels": {}
+}
 
-    tokens = line.split()
-    cmd = tokens[0]
-
-    if cmd == "coop":
-        name = tokens[1]
-        body = []
-        i += 1
-
-        while i < len(lines) and lines[i].strip() != "roost":
-            inner_line = lines[i].strip()
-
-            if not inner_line:
-                i += 1
-                continue
-
-            inner_tokens = inner_line.split()
-            inner_cmd = inner_tokens[0]
-
-            if inner_cmd == "bok":
-                body.append(("bok", inner_tokens[1]))
-            elif inner_cmd == "peck":
-                body.append(("peck", inner_tokens[1]))
-            elif inner_cmd == "cluck":
-                body.append(("cluck", inner_tokens[1]))
-            else:
-                print(f"Unknown command in loop: {inner_line}")
-
-            i += 1
-
-        statements.append(("loop", name, body))
-
-    elif cmd == "nest":
-        statements.append(("nest", tokens[1]))
-
-    elif cmd == "feed":
-        name = tokens[1]
-        value = count_clucks(tokens[2:])
-        statements.append(("feed", name, value))
-
-    elif cmd == "bok":
-        statements.append(("bok", tokens[1]))
-
-    elif cmd == "peck":
-        statements.append(("peck", tokens[1]))
-
-    elif cmd == "cluck":
-        statements.append(("cluck", tokens[1]))
-
-    else:
-        print(f"Unknown command: {line}")
-
-    i += 1
-
-    # print(statements) # <-- DEBUG STATEMENT
-
-    variables = {}
-    execute(statements, variables)
+execute(statements, state)
